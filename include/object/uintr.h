855c787f731d3ff8e87df7d158ea34e5408e88f5
JingXuan_Wei <137764115+J1ngxuanWei@users.noreply.github.com>
2025-02-26 16:56:59 +0800

    Add syscall register sender
    Signed-off-by: JingXuan_Wei <137764115+J1ngxuanWei@users.noreply.github.com>

--- include/object/uintr.h
+++ include/object/uintr.h
@@ -9,6 +9,7 @@
 #include <api/failures.h>
 #include <arch/types.h>
 #include <object/structures.h>
+#include <object/tcb.h>
 #include <arch/machine/fpu.h>
 
 typedef struct tcb tcb_t;
@@ -31,9 +32,10 @@ typedef struct tcb tcb_t;
 #define MSR_IA32_UINTR_STACKADJUST	0x987
 #define MSR_IA32_UINTR_MISC		0x988	/* 39:32-UINV, 31:0-UITTSZ */
 #define MSR_IA32_UINTR_PD		0x989
+#define MSR_IA32_UINTR_TT		0x98a
 #define UINTR_NOTIFICATION_VECTOR       0xec
 #define UINTR_MASK_1 0xFFFFFF00FFFFFFFF
-
+#define UINTR_MASK_2 0xFFFFFFFF00000000
 
 exception_t handle_SysUintrRegisterHandler(void);
 exception_t handle_SysUintrUnRegisterHandler(void);
@@ -48,6 +50,11 @@ exception_t handle_SysUintrIpiFd(void);
 inline bool_t is_uintr_receiver(tcb_t *t)
 {
 	return t->upid_activated;
+}
+
+inline bool_t is_uintr_sender(tcb_t *t)
+{
+	return t->uitt_activated;
 }
 
 /* TODO: UPID needs to be allocated by a KPTI compatible allocator */
@@ -67,4 +74,35 @@ static void alloc_upid(tcb_t *t)
 static inline void set_bit(int32_t nr, void *addr)
 {
 	asm("btsl %1,%0" : "+m" (*(uint32_t *)addr) : "Ir" (nr));
+}
+
+static void alloc_uitt(tcb_t *t)
+{
+	struct uintr_uitt_ctx *uitt_ctx = &t->uitt_ctx;
+	memset(uitt_ctx, 0, sizeof(struct uintr_uitt_ctx));
+
+	// TODOWJX: change to atomic operation
+	// refcount_set(&upid_ctx->refs, 1);
+	uitt_ctx->refs = 1;
+}
+
+static int32_t find_first_zero_bit(const uint64_t *addr, uint64_t size)
+{
+	uint64_t total_words = (size + 63) / 64;
+    uint64_t word_index = 0;
+    uint64_t bit_index = 0;
+    for (word_index = 0; word_index < total_words; word_index++) {
+        uint64_t word = addr[word_index];
+        for (bit_index = 0; bit_index < 64; bit_index++) {
+            if ((word & (1ULL << bit_index)) == 0) {
+                uint64_t position = word_index * 64 + bit_index;
+                if (position < size) {
+                	return position;
+                } else {
+                    return 256;
+                }
+            }
+        }
+    }
+    return 256;
 }
